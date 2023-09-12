302d53a39958dafca88d919928f0602e9c32502b
rennyson100a <rennysonlimadiasrennyson@gmail.com>
2023-09-11 23:04:28 -0300

    change in teste.py

--- teste.py
+++ teste.py
@@ -1,15 +1,25 @@
+import pymongo
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
+# Substitua as informações a seguir com os detalhes do seu servidor MongoDB remoto
 uri = "mongodb+srv://rennyson:rennyson100a@helpy.sllrgwg.mongodb.net/?retryWrites=true&w=majority"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+    cliente = pymongo.MongoClient(uri)
+
+    # A partir daqui, você pode continuar com as operações no banco de dados, conforme necessário.
+
+    # Exemplo de seleção de um banco de dados
+    db = cliente.get_database("seu_banco_de_dados")
+
+    # Exemplo de seleção de uma coleção
+    colecao = db.get_collection("sua_colecao")
+
+    # Faça as operações desejadas no banco de dados e coleção aqui.
+
+    # Feche a conexão quando terminar
+    cliente.close()
+    
+    print("Conexão bem-sucedida com o MongoDB remoto!")
+
+except pymongo.errors.ConnectionFailure as e:
+    print(f"Erro ao conectar ao MongoDB: {e}")
