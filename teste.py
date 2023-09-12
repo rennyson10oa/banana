c7fab8be2847602503550fd6a5daf857aa14dd48
rennyson100a <rennysonlimadiasrennyson@gmail.com>
2023-09-11 23:15:43 -0300

    outra atualizacão

--- teste.py
+++ teste.py
@@ -1,12 +1,9 @@
 import pymongo
 
-# Substitua as informações a seguir com os detalhes do seu servidor MongoDB remoto
 uri = "mongodb+srv://rennyson:rennyson100a@helpy.sllrgwg.mongodb.net/?retryWrites=true&w=majority"
 
 try:
     cliente = pymongo.MongoClient(uri)
-
-    # A partir daqui, você pode continuar com as operações no banco de dados, conforme necessário.
 
     # Exemplo de seleção de um banco de dados
     db = cliente.get_database("seu_banco_de_dados")
@@ -14,9 +11,8 @@ try:
     # Exemplo de seleção de uma coleção
     colecao = db.get_collection("sua_colecao")
 
-    # Faça as operações desejadas no banco de dados e coleção aqui.
+    # Fazer as operações desejadas no banco de dados e coleção aqui.
 
-    # Feche a conexão quando terminar
     cliente.close()
     
     print("Conexão bem-sucedida com o MongoDB remoto!")
