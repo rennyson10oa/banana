5375ffbd07c2b454714d85e3f97655b58b81f847
rennyson100a <rennysonlimadiasrennyson@gmail.com>
2023-09-15 12:43:53 -0300

    grande atu

--- helpy.py
+++ helpy.py
@@ -1,6 +1,32 @@
+from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, ScreenManager
+ScreenManager
+from kivymd.app import MDApp
+from kivy.uix.boxlayout import BoxLayout
 
 Window.size = (310, 580)
 
-class Happ(MDapp):
-    screen_manager = 
+
+class HomeWindow(Screen):
+    pass
+
+class LoginWindow(Screen):
+    pass
+
+class RegisterWindow(Screen):
+    pass
+
+class Happ(MDApp):
+    def build(self):
+        self.root = BoxLayout(orientation='vertical')
+
+
+        sm = ScreenManager()
+        sm.add_widget(HomeWindow(name='home'))
+        sm.add_widget(LoginWindow(name='login'))
+        sm.add_widget(RegisterWindow(name='register'))
+        return sm
+
+if __name__ == '__main__':
+    Happ().run()
