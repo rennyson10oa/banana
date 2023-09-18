780e572383cf5214fb2a2c5d11dd534e565ae062
rennyson100a <rennysonlimadiasrennyson@gmail.com>
2023-09-17 22:50:34 -0300

    ambatuikam

--- helpy.py
+++ helpy.py
@@ -1,3 +1,4 @@
+from kivy.core.text import LabelBase
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
@@ -13,19 +14,23 @@ class HomeWindow(Screen):
 class LoginWindow(Screen):
     pass
 
-class RegisterWindow(Screen):
+class SignupWindow(Screen):
     pass
 
 class Happ(MDApp):
     def build(self):
         self.root = BoxLayout(orientation='vertical')
 
-
         sm = ScreenManager()
-        sm.add_widget(HomeWindow(name='home'))
-        sm.add_widget(LoginWindow(name='login'))
-        sm.add_widget(RegisterWindow(name='register'))
+        sm.add_widget(Builder.load_file("hello.kv"))
+        sm.add_widget(Builder.load_file("signup.kv"))
+        sm.add_widget(Builder.load_file("login.kv"))
+        sm.add_widget(Builder.load_file("home.kv"))
         return sm
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    LabelBase.register(name="Poppins-Medium", fn_regular="C:\\Users\\renny\\Desktop\\banana\\assets\\fonts\\Poppins\\Poppins-Medium.ttf")
+    LabelBase.register(name="Poppins-SemiBold", fn_regular="C:\\Users\\renny\\Desktop\\banana\\assets\\fonts\\Poppins\\Poppins-SemiBold.ttf" )
+
     Happ().run()
