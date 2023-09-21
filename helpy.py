8f396527389c95d66aa686ed2f1f396c9ba7d576
rennyson100a <rennysonlimadiasrennyson@gmail.com>
2023-09-20 23:43:15 -0300

    atu2 day7

--- helpy.py
+++ helpy.py
@@ -8,12 +8,14 @@ from kivymd.uix.label import MDLabel
 
 Window.size = (310, 580)
 
-
 class HomeWindow(Screen):
     pass
 
 class LoginWindow(Screen):
-    pass
+    def __init__(self, **kwargs):
+        from firebase import firebase
+        super(LoginWindow, self).__init__(**kwargs)
+        #chama o banco de dados
 
 class SignupWindow(Screen):
     pass
@@ -28,13 +30,12 @@ class Happ(MDApp):
         self.sm.add_widget(Builder.load_file("login.kv"))
         self.sm.add_widget(Builder.load_file("home.kv"))
         return self.sm
-    def send_data(self, email, password):
+    
+    def login(self, email, password):
         from firebase import firebase
-        #chama banco de dados
-        self.error_label = Label(text='', color=(1, 0, 0, 1))  # Label para a mensagem de erro
-        self.add_widget(self.error_label)  # Adicionar o Label à tela
-        firebase = firebase.FirebaseApplication('https://helpyweather-b54ab-default-rtdb.firebaseio.com', None)
 
+        firebase = firebase.FirebaseApplication('https://helpyweather-b54ab-default-rtdb.firebaseio.com', None)
+        #pega o resutado do banco de dados
         
         result = firebase.get('https://helpyweather-b54ab-default-rtdb.firebaseio.com/Users', '')
         
@@ -42,19 +43,19 @@ class Happ(MDApp):
             for i in result.keys():
                 if result[i]['Email'] == email:
                     if result[i]['Password'] == password:
-                        print(email, "logado com sucesso")
                         self.sm.current = 'home'
         except Exception as e:
+            self.error_label = self.ids.error_label
             self.error_label.text = f"Erro de autenticação: {e}"  # Exibir mensagem de erro
             Clock.schedule_once(self.clear_error, 5)
     
     def clear_error(self, dt):
-        self.error_label.text = ''
+        error_label = self.ids.error_label
+        error_label.text = ''
+
                 
 
 if __name__ == "__main__":
-
     LabelBase.register(name="Poppins-Medium", fn_regular="C:\\Users\\renny\\Desktop\\banana\\assets\\fonts\\Poppins\\Poppins-Medium.ttf")
     LabelBase.register(name="Poppins-SemiBold", fn_regular="C:\\Users\\renny\\Desktop\\banana\\assets\\fonts\\Poppins\\Poppins-SemiBold.ttf" )
-
     Happ().run()
