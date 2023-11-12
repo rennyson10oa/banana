43ff97ef52658e2dcb02ab2a59abf0e0064129c3
Rennyson100a <73185376+rennyson10oa@users.noreply.github.com>
2023-11-12 00:00:39 -0300

    Update arduino.cpp

--- arduino.cpp
+++ arduino.cpp
@@ -1,33 +1,56 @@
-#include <DHT.h>
-
-const int dhtPin = 4;  // GPIO4 para DHT11
-const int higroPin = A0;
-int higroValue = 0; // valor inicial
-
-DHT dht(dhtPin, DHT11);
+#include <ESP8266WiFi.h>
 
 void setup() {
   Serial.begin(115200);
-  dht.begin();
+
+  // Inicie a conexão Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(1000);
+  Serial.println("Escaneando redes Wi-Fi próximas:");
+  int numRedes = WiFi.scanNetworks();
+
+  if (numRedes == 0) {
+    Serial.println("Nenhuma rede Wi-Fi encontrada.");
+  } else {
+    Serial.println("Redes Wi-Fi disponíveis:");
+    for (int i = 0; i < numRedes; i++) {
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.println(WiFi.SSID(i));
+    }
+
+    Serial.print("Escolha o número da rede Wi-Fi: ");
+    while (Serial.available() == 0) {
+      // Aguarda a entrada do usuário
+    }
+
+    int escolhaRede = Serial.parseInt();
+    if (escolhaRede > 0 && escolhaRede <= numRedes) {
+      Serial.print("Digite a senha da rede escolhida: ");
+      while (Serial.available() == 0) {
+        // Aguarda a entrada do usuário
+      }
+      String senha = Serial.readString();
+      senha.trim();
+
+      Serial.print("Conectando à rede: ");
+      Serial.println(WiFi.SSID(escolhaRede - 1));
+
+      WiFi.begin(WiFi.SSID(escolhaRede - 1).c_str(), senha.c_str());
+
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Conectando ao WiFi...");
+      }
+
+      Serial.println("Conectado à rede WiFi");
+    } else {
+      Serial.println("Número de rede inválido.");
+    }
+  }
 }
 
 void loop() {
-  // Read the temperature and humidity data from the sensor
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  higroValue = analogRead(higroPin);
-
-  // Print the data to the serial monitor
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println(" C");
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println(" %");
-  Serial.print("sensor = ");
-  Serial.print(higroValue);
-  
-
-  // Wait for 2 seconds before taking the next reading
-  delay(2000);
+  // Seu código principal aqui
 }
