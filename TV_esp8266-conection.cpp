8d2b1e104e5aa4906fc03e33cf78f41f7997b1f8
Rennyson100a <73185376+rennyson10oa@users.noreply.github.com>
2023-11-26 23:13:25 -0300

    Update TV_esp8266-conection.cpp

--- TV_esp8266-conection.cpp
+++ TV_esp8266-conection.cpp
@@ -1,61 +1,67 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+#include <ArduinoJson.h>
 #include <DHT.h>
 
-// Substitua essas informações com as suas credenciais Firebase
-#define FIREBASE_HOST "helpyweather-b54ab-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "LwDhQynOI2BrXFBzxkSW8GSkUF9zAEqVL1nCK6Ck"
+// Set these to run example. 
+#define FIREBASE_HOST "helpyweather-b54ab-default-rtdb.firebaseio.com" 
+#define FIREBASE_AUTH "LwDhQynOI2BrXFBzxkSW8GSkUF9zAEqVL1nCK6Ck" 
+#define WIFI_SSID "LIVECOM_JOSE" 
+#define WIFI_PASSWORD "9677tjsdias"
 
-// Substitua essas informações com as suas credenciais de Wi-Fi
-#define WIFI_SSID "wifi"
-#define WIFI_PASSWORD "senha"
-
-#define DHT_PIN 4 // Pino de dados do sensor DHT
-const int pinHigrometro = A0; 
+#define DHT_PIN 4
+const int pinHigrometro = A0;
 
 DHT dht(DHT_PIN, DHT11);
 
-void setup() {
-  Serial.begin(115200);
+void setup() { 
 
-  // Conecta-se ao Wi-Fi
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conectando ao WiFi...");
-  }
-  Serial.println("Conectado ao Wi-Fi!");
-
+  Serial.begin(9600); 
   // Inicializa o sensor DHT
   dht.begin();
 
-  // Inicializa a comunicação com o Firebase
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-}
+  // connect to wifi. 
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); 
+  Serial.print("connecting"); 
+  while (WiFi.status() != WL_CONNECTED) { 
+    Serial.print("."); 
+    delay(500); 
+  } 
+  Serial.println(); 
+  Serial.print("connected: "); 
+  Serial.println(WiFi.localIP()); 
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); 
+} 
 
-void loop() {
+void loop() { 
   // Lê os valores do sensor DHT
   float temperatura = dht.readTemperature();
-  float umidade = dht.readHumidity();
+  float umidadeAr = dht.readHumidity();
 
   // Simula um valor para o sensor de solo (substitua com a lógica do seu sensor)
   int umidadeSolo = analogRead(pinHigrometro);
 
-  Serial.print("Temperatura: ");
-  Serial.print(temperatura);
-  Serial.print(" °C, Umidade: ");
-  Serial.print(umidade);
-  Serial.print("%, Umidade do Solo: ");
-  Serial.println(umidadeSolo);
-
-  // Cria um objeto JSON para enviar ao Firebase
-  String jsonData = "{\"temperatura\": " + String(temperatura) +
-                    ", \"umidade\": " + String(umidade) +
-                    ", \"umidade_solo\": " + String(umidadeSolo) + "}";
-
-  // Envia os dados para a coleção "dados" no Firebase
-  Firebase.pushString("/dados", jsonData);
-
-  // Aguarda por um intervalo antes de enviar novamente (por exemplo, a cada 5 segundos)
-  delay(5000);
+  // set value 
+  Firebase.setFloat("sensores/umidadeSolo", umidadeSolo); 
+  // handle error 
+  if (Firebase.failed()) { 
+      Serial.print("setting /number failed:"); 
+      Serial.println(Firebase.error());   
+      return; 
+  }
+  Firebase.setFloat("sensores/umidadeAr", umidadeAr); 
+  // handle error 
+  if (Firebase.failed()) { 
+      Serial.print("setting /number failed:"); 
+      Serial.println(Firebase.error());   
+      return; 
+  } 
+  Firebase.setFloat("sensores/temperatura", temperatura); 
+  // handle error 
+  if (Firebase.failed()) { 
+      Serial.print("setting /number failed:"); 
+      Serial.println(Firebase.error());   
+      return; 
+  } 
+  delay(1000); 
 }
